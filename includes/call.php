ea1e76f8c4530c15350cfab72e95487f07c8f5c8
Ivan Ruiz Delfin <ivanruizdelfin@gmail.com>
2020-08-30 19:34:36 -0400

    se borraron datos de la cuenta voximplan

--- includes/call.php
+++ includes/call.php
@@ -1,52 +1,70 @@
-<?
+<?php
 
-$vox_nombre     = "piero gallegos";
-$vox_telefono   = "988352651";
+    $nombre			= $_POST['nombre'];
+    $pais			  = $_POST['pais'];
+    $telefono		= $_POST['telefono'];
+    $email			= $_POST['email'];
 
-$vox_phone = "+56".$vox_telefono;
+    if ($pais == 'CH'){
 
-// Your Voximplant Kit account name
-define('KIT_DOMAIN', "Piero");
+      $codigo_pais = "+56";
 
-// API Token
-define('KIT_ACCESS_TOKEN', "be5880fb72d7285513701113755089c1");
+    }elseif ($codigo_pais == 'PE') {
 
-// ID of your CallerID number ID
-define('KIT_CALLERID_PHONE_ID', "1684");
+      $codigo_pais = "+51";
 
-// Voximplant Kit scenario id
-define('KIT_SCENARIO_ID', "15109");
+    }else {
 
-// Voximplant Kit API url
-define('KIT_API_URL', "https://kit.voximplant.com/api/v3/");
+      $codigo_pais = "+57";
+    }
 
-// Client data for using in scenario
-$client_data = [
-    'phone' => $vox_phone,
-    'client_name' => $vox_nombre
-];
 
-// API Request parameters
-$run_scenario_data = [
-    'domain' => KIT_DOMAIN,
-    'access_token' => KIT_ACCESS_TOKEN,
-    'scenario_id' => KIT_SCENARIO_ID,
-    'phone_number_id' => KIT_CALLERID_PHONE_ID,
-    'phone' => $client_data['phone'],
-    'variables' => json_encode([
-        'client_name' => $client_data['client_name']
-    ])
-];
+    $vox_nombre     = $nombre;
+    $vox_telefono   = $telefono;
+    $vox_phone      = $codigo_pais.$vox_telefono;
 
-// HTTP Request to run the scenario
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, KIT_API_URL . "scenario/runScenario");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $run_scenario_data);
-$returned = curl_exec($ch);
-curl_close ($ch);
+    // Nombre usuario Voximplant Kit
+    define('KIT_DOMAIN', "");
 
-echo $returned;
+    // API Token
+    define('KIT_ACCESS_TOKEN', "");
+
+    // Id numero contratado de salida
+    define('KIT_CALLERID_PHONE_ID', "");
+
+    // Id del Escenario a llamar
+    define('KIT_SCENARIO_ID', "");
+
+    // Voximplant Kit API url
+    define('KIT_API_URL', "https://kit.voximplant.com/api/v3/");
+
+    // Client data for using in scenario
+    $client_data = [
+        'phone' => $vox_phone,
+        'client_name' => $vox_nombre
+    ];
+
+    // API Request parameters
+    $run_scenario_data = [
+        'domain' => KIT_DOMAIN,
+        'access_token' => KIT_ACCESS_TOKEN,
+        'scenario_id' => KIT_SCENARIO_ID,
+        'phone_number_id' => KIT_CALLERID_PHONE_ID,
+        'phone' => $client_data['phone'],
+        'variables' => json_encode([
+            'client_name' => $client_data['client_name']
+        ])
+    ];
+
+    // HTTP Request to run the scenario
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, KIT_API_URL . "scenario/runScenario");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $run_scenario_data);
+    $returned = curl_exec($ch);
+    curl_close ($ch);
+
+    echo "1";
 
 ?>
